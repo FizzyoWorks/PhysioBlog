ad97af9d502ff0262f3a3d15ec8a908fd6601022
Mark <mark.stacey@aphelion.bi>
2019-11-08 12:16:34 +0000

    COnfig

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,28 @@ module.exports = {
     theme: '@vuepress/blog',
     themeConfig: {
         // Please keep looking down to see the available options
+        nav: [
+            {
+              text: 'Home',
+              link: '/',
+            },
+            {
+              text: 'Archive',
+              link: '/archive/',
+            },
+            {
+              text: 'Tags',
+              link: '/tag/',
+            },
+          ],
+          footer: {
+            contact: [
+              {
+                type: 'instagram',
+                link: 'https://instagram.com/footiephysio',
+              },
+            ],
+          },
     },
     plugins: [
         [
@@ -14,7 +36,12 @@ module.exports = {
                         // Target directory
                         dirname: '_posts',
                         // Path of the 'entry page' (or 'list page')
-                        path: '/'
+                        path: '/',
+                        itemLayout: 'Post',
+                        itemPermalink: '/:year/:month/:day/:slug',
+                        pagination: {
+                            lengthPerPage: 5,
+                          },
                     }
                 ]
             }
